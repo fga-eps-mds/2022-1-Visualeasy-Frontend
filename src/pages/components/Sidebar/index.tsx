c22ede014b937060855e7e8834bbc55852d77f02
damarcones <damarcones@gmail.com>
2022-08-23 23:52:54 -0300

    Adiciona painel de abas
    Co-authored-by: brunaalmeidasantos <brunaalmeida48@gmail.com>
    Co-authored-by: lbrunofidelis <l.brunofidelis@gmail.com>
    Co-authored-by: Itallo Gravina <itallo.gravina@gmail.com>

--- src/pages/components/Sidebar/index.tsx
+++ src/pages/components/Sidebar/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
 
   Image,
@@ -9,16 +9,57 @@ import {
   Tab,
   TabPanels,
   TabPanel,
-  Center
+  Center,
+  Button
 } from "@chakra-ui/react";
+
+import { AiOutlineClose } from 'react-icons/ai';
 
 import FormGraph from "../FormGraph";
 
-export default function Sidebar({SidebarData}:any) {
-  
+export default function Sidebar({ SidebarData }: any) {
+  const [numTabs, setNumTabs] = useState(1);
+  const [mostrarSegundaAba, setMostrarSegundaAba] = useState(false);
+  const [mostrarTerceiraAba, setMostrarTerceiraAba] = useState(false);
+  const [mostrarQuartaAba, setMostrarQuartaAba] = useState(false);
+
+  useEffect(() => {
+    console.log(numTabs);
+  }, [numTabs])
+
+  function addTab() {
+    if (numTabs === 1) {
+      setMostrarSegundaAba(true);
+    } else if (numTabs === 2) {
+      setMostrarTerceiraAba(true);
+    } else if (numTabs === 3) {
+      setMostrarQuartaAba(true);
+    }
+
+    if (numTabs <= 4) {
+      setNumTabs(numTabs + 1);
+    }
+  }
+
+  function renderNewTab() {
+    console.log("new tab")
+    return (
+      <Tab>
+        Gráfico 1
+        {numTabs === 1 ?
+          <div></div>
+          :
+          <Button onClick={() => setNumTabs(numTabs - 1)} size="sm">
+            <AiOutlineClose />
+          </Button>
+        }
+      </Tab>
+    );
+  }
+
   return (
 
-    <VStack align='stretch'w="340px">
+    <VStack align='stretch' w="340px">
       <Box marginBottom="10px">
         <Center>
           <Image
@@ -28,14 +69,103 @@ export default function Sidebar({SidebarData}:any) {
           />
         </Center>
       </Box>
-      <Tabs  className="Tabs" variant='enclosed'>
-        <TabList>
-          <Tab>Gráfico 1</Tab>
+      <Tabs isFitted defaultIndex={0} className="Tabs" variant='enclosed'>
+        <TabList mb="1em">
+          <Tab>
+            Gráfico 1
+            {numTabs === 1 ?
+              <div></div>
+              :
+              <Button onClick={() => setNumTabs(numTabs - 1)} size="sm">
+                <AiOutlineClose />
+              </Button>
+            }
+          </Tab>
+
+          {
+            mostrarSegundaAba ?
+              <Tab>
+                Gráfico 2
+                {
+                  numTabs === 1 ?
+                    <div></div>
+                    :
+                    <Button onClick={() => { setNumTabs(numTabs - 1); setMostrarSegundaAba(false) }} size="sm">
+                      <AiOutlineClose />
+                    </Button>
+                }
+              </Tab>
+              :
+              <div></div>
+          }
+
+          {
+            mostrarTerceiraAba ?
+              <Tab>
+                Gráfico 3
+                {
+                  numTabs === 1 ?
+                    <div></div>
+                    :
+                    <Button onClick={() => { setNumTabs(numTabs - 1); setMostrarTerceiraAba(false) }} size="sm">
+                      <AiOutlineClose />
+                    </Button>
+                }
+              </Tab>
+              :
+              <div></div>
+          }
+
+          {
+            mostrarQuartaAba ?
+              <Tab>
+                Gráfico 4
+                {
+                  numTabs === 1 ?
+                    <div></div>
+                    :
+                    <Button onClick={() => { setNumTabs(numTabs - 1); setMostrarQuartaAba(false) }} size="sm">
+                      <AiOutlineClose />
+                    </Button>
+                }
+              </Tab>
+              :
+              <div></div>
+          }
+          <Button colorScheme='red' size='md' onClick={addTab}> + </Button>
         </TabList>
+
         <TabPanels>
           <TabPanel>
-            <FormGraph FormGraphProps={(e)=>{SidebarData({...e})}} />
+            <FormGraph FormGraphProps={(e) => { SidebarData({ ...e }) }} />
           </TabPanel>
+
+          {
+            mostrarSegundaAba ?
+              <TabPanel>
+                <FormGraph FormGraphProps={(e) => { SidebarData({ ...e }) }} />
+              </TabPanel>
+              :
+              <div></div>
+          }
+
+          {
+            mostrarTerceiraAba ?
+              <TabPanel>
+                <FormGraph FormGraphProps={(e) => { SidebarData({ ...e }) }} />
+              </TabPanel>
+              :
+              <div></div>
+          }
+
+          {
+            mostrarQuartaAba ?
+              <TabPanel>
+                <FormGraph FormGraphProps={(e) => { SidebarData({ ...e }) }} />
+              </TabPanel>
+              :
+              <div></div>
+          }
         </TabPanels>
       </Tabs>
     </VStack>
