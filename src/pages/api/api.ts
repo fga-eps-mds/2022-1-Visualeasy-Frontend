3141f83a8a329c2f91ccd1524a99aafe775bd49b
gustavoduartemoreira <gustavoduartemoreira@gmail.com>
2022-10-24 15:10:28 -0300

    Remove Unnecessary semicolon.

--- src/pages/api/api.ts
+++ src/pages/api/api.ts
@@ -2,38 +2,36 @@ import axios from "axios";
 
 axios.defaults.baseURL = process.env.NEXT_PUBLIC_BASE_URL;
 export interface Variavel {
-    id?: number,
-    variavel?: string;
-    data?: Date;
-    valor?: number;
+  id?: number;
+  variavel?: string;
+  data?: Date;
+  valor?: number;
 }
-export interface Variavel extends Array<Variavel> { }
+export interface Variavel extends Array<Variavel> {}
 
 const paramstype = {
-    variavel: "Bulbassauro",
-    startDate: '2022-06-30T06:18:50',
-    endDate: '2022-06-30T06:26:14'
-}
-export async function listaNomeVariaveis() {
-    try {
-        const resposta = await axios.get("variavel/");
-        return resposta.data;
-    } catch (e) {
-        return e
-    }
-
+  variavel: "Bulbassauro",
+  startDate: "2022-06-30T06:18:50",
+  endDate: "2022-06-30T06:26:14",
 };
+export async function listaNomeVariaveis() {
+  try {
+    const resposta = await axios.get("variavel/");
+    return resposta.data;
+  } catch (e) {
+    return e;
+  }
+}
 
 export const postAllData = async (params: any, dataapi: any) => {
-    try {
-
-        const { data }: any = await axios.post(`/variavel/${params}`, dataapi).catch(function (error) {
-            console.error(error);
-        });
-        return data
-
-    } catch (e) {
-        return e
-    }
-}
-
+  try {
+    const { data }: any = await axios
+      .post(`/variavel/${params}`, dataapi)
+      .catch(function (error) {
+        console.error(error);
+      });
+    return data;
+  } catch (e) {
+    return e;
+  }
+};
